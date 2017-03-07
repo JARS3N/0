a8e5b7b802a3b6cd264d79ea02182b5f83f41237
JARS3N <JARS3N@users.noreply.github.com>
2017-03-07 16:39:26 -0500

    Update forceUp.R

--- R/forceUp.R
+++ R/forceUp.R
@@ -4,3 +4,11 @@ pkginfo<-packageDescription(pkg)
 repo<-file.path(pkginfo$GithubUsername,pkginfo$GithubRepo)
 devtools::install_github(repo,force=TRUE)
 }
+
+
+forceUp2<-function(){
+pkg<-environment(deparse(sys.call()[[1]]))$.packageName
+pkginfo<-packageDescription(pkg)
+repo<-file.path(pkginfo$GithubUsername,pkginfo$GithubRepo)
+list(pkg=pkg,pkginfo=pkginfo,repo=repo)
+}
