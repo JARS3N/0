27a8507556aa98043f92d2ef5c467bab6edbe6e6
JARS3N <JARS3N@users.noreply.github.com>
2017-03-07 15:39:38 -0500

    Update forceUp.R

--- R/forceUp.R
+++ R/forceUp.R
@@ -1,8 +1,8 @@
 forceUp<-function(){
   require(magrittr)
-  deparse(sys.call()[[1]]) %>% 
-  environment(fun=.) %>% 
-  packageDescription(.) %>% 
-  file.path(.$GithubUsername,.$GithubRepo) #%>% 
+  deparse(sys.call()[[1]])# %>% 
+ # environment(fun=.) %>% 
+ # packageDescription(.) %>% 
+ # file.path(.$GithubUsername,.$GithubRepo) #%>% 
   #devtools::install_github(repo=.,force=TRUE)
 }
