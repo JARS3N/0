6a2d28593e5716f4bbaca250544aa95a8c31c7c7
JARS3N <JARS3N@users.noreply.github.com>
2017-03-08 00:44:06 -0500

    Update forceUp.R

--- R/forceUp.R
+++ R/forceUp.R
@@ -1,23 +1,7 @@
-
-
-forceUp2<-function(){
-  require(magrittr)
-deparse(sys.call()[[1]]) %>%
-gsub("[:]{2}[a-z,A-Z,0-9]+","",.)
-#strsplit(.,"[:]{2}") #%>% 
- # unlist() %>%
- # .[1]
-#strsplit(.,"::")[[1]][1] #%>%
-#packageDescription(.)%>%
-#file.path(.$GithubUsername,.$GithubRepo)#%>%
-#list(pkg=pkg,pkginfo=pkginfo,repo=repo)
-}
-
-
-
 forceUp<-function(){
 require(magrittr)
 pkg<-gsub("[:]{2}[A-Z|a-z|0-9]+","",deparse(sys.call()[[1]])) 
 info<-packageDescription(pkg)  
-file.path(info$GithubUsername,info$GithubRepo)  
+fl<-file.path(info$GithubUsername,info$GithubRepo)  
+ devtools::install_github(repo=fl,force=T)
 }
