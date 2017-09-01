cf9f879ba3f03cb63a07d8e609683ef6fc27e37a
JARS3N <JARS3N@users.noreply.github.com>
2017-09-01 10:53:38 -0400

    Update forceUp.R

--- R/forceUp.R
+++ R/forceUp.R
@@ -1,5 +1,5 @@
 forceUp<-function(){
-require(magrittr)
+#require(magrittr)
 pkg<-gsub("[:]{2}[A-Z|a-z|0-9]+","",deparse(sys.call()[[1]])) 
 info<-packageDescription(pkg)  
 fl<-file.path(info$GithubUsername,info$GithubRepo)  
